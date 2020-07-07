cd9ab5b31059222faf7cc57e6397f4658efb0007
nelmie <nelmie.turla@gmail.com>
2020-07-07 08:52:18 +0000

    Remove old stuff

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '111aovqw(7ga#tlsqfnn%8&*0q2(f--q895(3)sw_kzin_yu2&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nelmie07.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
